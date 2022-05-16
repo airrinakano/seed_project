c2b4427f85ceeeb04ef5a42bc3b4f3c1619d7355
“airrinakano” <airinakano0212@gmail.com>
2022-05-16 21:10:38 +0900

    frontend ポート番号変更

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  transpileDependencies: true,
+  configureWebpack: {
+    watch: true,
+  },
+  devServer: {
+    port: 8082,
+  },
 });
