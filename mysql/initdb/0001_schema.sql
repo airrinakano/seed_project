5f0b56330af8f18e379b1a421c3a848c7ff25ce7
“airrinakano” <airinakano0212@gmail.com>
2022-05-23 20:53:42 +0900

    DB設計

--- mysql/initdb/0001_schema.sql
+++ mysql/initdb/0001_schema.sql
@@ -1,30 +1,86 @@
 DROP SCHEMA IF EXISTS mysqldb;
 CREATE SCHEMA mysqldb;
 use mysqldb;
--- -----------------------------------------------------
--- 以下　アカウント関連マスタテーブル
--- ----------------------------------------------------
+
 -- -----------------------------
--- COMPANY
+--  PRODUCT
 -- -----------------------------
-CREATE TABLE IF NOT EXISTS m_company(
-  company_id INTEGER NOT NULL AUTO_INCREMENT COMMENT '会社組織ID',
-  company_name VARCHAR(65) NOT NULL COMMENT '会社組織名称',
-  system_login VARCHAR(65) NOT NULL COMMENT 'システムログイン',
-  shared_secret VARCHAR(65) NOT NULL COMMENT 'カタログ連携パスワード',
-  po_url VARCHAR(128) NOT NULL COMMENT 'POのURL',
-  cxml_id VARCHAR(65) NOT NULL COMMENT 'cxmlID',
-  cxml_password VARCHAR(65) NOT NULL COMMENT 'cxmlパスワード',
+CREATE TABLE IF NOT EXISTS m_product(
+  product_id INTEGER NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  product_name VARCHAR(65) NOT NULL COMMENT '商品名',
+  unit_price INTEGER NOT NULL COMMENT '単価',
+  tax INTEGER NOT NULL COMMENT '商品税',
   created_by VARCHAR(20) NOT NULL COMMENT '作成者',
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
   updated_by VARCHAR(20) NOT NULL COMMENT '最終更新者',
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最終更新日時',
   delete_flag boolean NOT NULL DEFAULT false COMMENT '終了・無効フラグ',
-  INDEX(company_name),
   INDEX(created_by),
   INDEX(created_at),
   INDEX(updated_by),
   INDEX(updated_at),
   INDEX(delete_flag),
-  PRIMARY KEY (company_id)
+  PRIMARY KEY (product_id)
+);
+
+-- -----------------------------
+-- MEMBER
+-- -----------------------------
+CREATE TABLE IF NOT EXISTS m_member(
+  -- EC-CUBEでの型等確認
+  member_id VARCHAR(20) NOT NULL COMMENT '会員ID',
+  first_name VARCHAR(30) NOT NULL COMMENT '名',
+  last_name VARCHAR(30) NOT NULL COMMENT '姓',
+  pass_word VARCHAR(30) NOT NULL COMMENT 'パスワード',
+  init_email VARCHAR(65) NOT NULL COMMENT '登録時メールアドレス',
+  address_name VARCHAR(65) COMMENT '住所',
+  created_by VARCHAR(20) NOT NULL COMMENT '作成者',
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  updated_by VARCHAR(20) NOT NULL COMMENT '最終更新者',
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最終更新日時',
+  delete_flag boolean NOT NULL DEFAULT false COMMENT '終了・無効フラグ',
+  display_system_maintenance_notice_flag boolean NOT NULL DEFAULT true COMMENT 'システムメンテナンス通知表示フラグ',
+  INDEX(created_by),
+  INDEX(created_at),
+  INDEX(updated_by),
+  INDEX(updated_at),
+  INDEX(delete_flag),
+  PRIMARY KEY (member_id)
+);
+
+-- -----------------------------
+-- ORDER INFO
+-- -----------------------------
+CREATE TABLE IF NOT EXISTS t_order_info (
+  -- 主キー
+  order_id VARCHAR(19) NOT NULL COMMENT 'orderID',
+  member_id VARCHAR (20) NOT NULL COMMENT '購入者ID',
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最終更新日時',
+  updated_by VARCHAR(20) NOT NULL COMMENT '最終更新者',
+  INDEX(created_at),
+  INDEX(updated_at),
+  INDEX(updated_by),
+  PRIMARY KEY (order_id),
+  FOREIGN KEY(member_id) REFERENCES m_member(member_id) ON DELETE RESTRICT
+);
+
+-- -----------------------------
+-- ORDER Detail
+-- -----------------------------
+CREATE TABLE IF NOT EXISTS t_order_detail (
+  -- 主キー
+  order_id VARCHAR(19) NOT NULL COMMENT '注文ID',
+  line_number INTEGER NOT NULL COMMENT 'カート内のシーケンス番号',
+  -- 本文
+  quantity INTEGER NOT NULL COMMENT '量',
+  product_id INTEGER NOT NULL COMMENT '商品ID',
+  unit_price INTEGER NOT NULL COMMENT '単価',
+  shipping_fee INTEGER NOT NULL COMMENT '商品単位送料',
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  INDEX(created_at),
+  PRIMARY KEY (order_id, line_number),
+  -- ORDER INFORMATIONとの紐づけ
+  FOREIGN KEY(order_id) REFERENCES t_order_info(order_id) ON DELETE RESTRICT,
+  FOREIGN KEY(product_id) REFERENCES m_product(product_id) ON DELETE RESTRICT
 );
